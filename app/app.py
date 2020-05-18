4dd481269e9e5828d90db73f857c716e996d21c0
Peter Curet <petercuret@gmail.com>
2020-05-18 14:40:05 +0200

    Added a simple health check

--- app/app.py
+++ app/app.py
@@ -8,6 +8,12 @@ app = Flask(__name__)
 def hello():
     return 'Hello World! {}'.format(time.time())
 
+@app.route('/health')
+def health_check():
+    """
+    A tentative health check
+    """
+    return 'Connectivity OK'
 
 if __name__ == '__main__':  # pragma: no cover
     app.run()
