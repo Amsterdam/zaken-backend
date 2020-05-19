571ef1e4ede9db3ba075a863adcdc849340ada6f
Peter Curet <petercuret@gmail.com>
2020-05-19 16:52:54 +0200

    Experimental demo of flask & open-zaak

--- app/app.py
+++ app/app.py
@@ -1,12 +1,32 @@
-import time
+# Very experimental proof of concept for a simple flask app and a connected open-zaak app
 
-from flask import Flask
+import time
+import requests
+from flask import Flask, Response
 
 app = Flask(__name__)
 
-@app.route('/')
+CASES_PATH = 'http://openzaak:8000'
+STATIC_PATH = 'static-sub/static'
+
+@app.route('/test')
 def hello():
     return 'Hello World! {}'.format(time.time())
+
+@app.route('/')
+def cases():
+    return requests.get(CASES_PATH).text
+
+@app.route('/{}/<path:path>'.format(STATIC_PATH))
+def static_files(path):
+    path = '{}/{}/{}'.format(CASES_PATH, STATIC_PATH, path)
+    r = requests.get(path)
+    return Response(response=r.text, status=r.status_code, content_type=r.headers.get('content-type', ''))
+
+@app.route('/<path:path>')
+def cases_sub(path):
+    path = '{}/{}'.format(CASES_PATH, path)
+    return requests.get(path).text
 
 @app.route('/health')
 def health_check():
