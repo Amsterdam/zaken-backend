456055d0c01c189288767d52bb0a11fc8b3acdcc
Peter Curet <petercuret@gmail.com>
2020-05-20 11:39:16 +0200

    Revert experimental POC. Runs two container on separate ports now, zo open-zaak can be accessed for prototyping purposes.

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-# Very experimental proof of concept for a simple flask app and a connected open-zaak app
+
 
 import time
 import requests
@@ -6,27 +6,9 @@ from flask import Flask, Response
 
 app = Flask(__name__)
 
-CASES_PATH = 'http://openzaak:8000'
-STATIC_PATH = 'static-sub/static'
-
-@app.route('/test')
-def hello():
-    return 'Hello World! {}'.format(time.time())
-
 @app.route('/')
-def cases():
-    return requests.get(CASES_PATH).text
-
-@app.route('/{}/<path:path>'.format(STATIC_PATH))
-def static_files(path):
-    path = '{}/{}/{}'.format(CASES_PATH, STATIC_PATH, path)
-    r = requests.get(path)
-    return Response(response=r.text, status=r.status_code, content_type=r.headers.get('content-type', ''))
-
-@app.route('/<path:path>')
-def cases_sub(path):
-    path = '{}/{}'.format(CASES_PATH, path)
-    return requests.get(path).text
+def index():
+    return 'Hello World! {}'.format(time.time())
 
 @app.route('/health')
 def health_check():
@@ -37,3 +19,19 @@ def health_check():
 
 if __name__ == '__main__':  # pragma: no cover
     app.run()
+
+# Very experimental proof of concept for a simple flask app and a connected open-zaak app
+# Disabled for now
+# CASES_PATH = 'http://openzaak:8000'
+# STATIC_PATH = 'static-sub/static'
+# @app.route('/{}/<path:path>'.format(STATIC_PATH))
+# def static_files(path):
+#     path = '{}/{}/{}'.format(CASES_PATH, STATIC_PATH, path)
+#     r = requests.get(path)
+#     return Response(response=r.text, status=r.status_code, content_type=r.headers.get('content-type', ''))
+#
+# @app.route('/<path:path>')
+# def cases_sub(path):
+#     path = '{}/{}'.format(CASES_PATH, path)
+#     return requests.get(path).text
+
