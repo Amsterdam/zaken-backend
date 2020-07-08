1d44a501a0975d6119f3cd97914ac70abc8a58e5
Peter Curet <petercuret@gmail.com>
2020-07-08 16:13:29 +0200

    Pre commit using yaml configuration

--- app/development/pre-commit.sh
+++
@@ -1,5 +0,0 @@
-echo Running pre-commit
-pip3 install black
-pip3 install isort
-black ./app
-isort ./app/**/*.py
--- install.sh
+++ install.sh
@@ -1,3 +1,2 @@
-echo Installing pre-commit hooks
-rm -f .git/hooks/pre-commit
-ln -s app/development/pre-commit.sh .git/hooks/pre-commit
+pip3 install pre-commit
+pre-commit install
