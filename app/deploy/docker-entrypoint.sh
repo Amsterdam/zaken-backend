514ece76eeda3a704f7398bcca45d850d46f3191
Peter Curet <petercuret@gmail.com>
2020-07-09 12:48:29 +0200

    Updated entrypoint wait loop

--- app/deploy/docker-entrypoint.sh
+++ app/deploy/docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -u   # crash on missing env variables
 set -e   # stop on any error
 set -x
 
-until PGPASSWORD=$DATABASE_PASSWORD psql -h "database" -U $DATABASE_USER -c '\q'; do
+until PGPASSWORD=$DATABASE_PASSWORD psql -h $DATABASE_HOST -U $DATABASE_USER -c '\q'; do
   echo "Postgres is unavailable - sleeping"
   sleep 1
 done
