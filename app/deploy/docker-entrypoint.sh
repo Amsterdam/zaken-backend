a2f98986067fc65fa043d9e5f74259087f37c259
Peter Curet <petercuret@gmail.com>
2020-07-10 13:54:34 +0200

    Fix

--- app/deploy/docker-entrypoint.sh
+++ app/deploy/docker-entrypoint.sh
@@ -18,7 +18,7 @@ python manage.py migrate --noinput
 
 echo Create root user
 
-python manage.py shell -c "from django.db import connection; cursor = connection.cursor(); cursor.execute('ALTER TABLE django_admin_log DROP COLUMN user_id'); cursor.close();"
+# python manage.py shell -c "from django.db import connection; cursor = connection.cursor(); cursor.execute('ALTER TABLE django_admin_log DROP COLUMN user_id'); cursor.close();"
 python manage.py shell -c "from django.db import connection; cursor = connection.cursor(); cursor.execute('ALTER TABLE django_admin_log ADD COLUMN user_id UUID NOT NULL'); cursor.close();"
 python manage.py shell -c "from apps.users.models import User; User.objects.all().delete()"
 python manage.py shell -c "from apps.users.models import User; User.objects.create_superuser('admin@admin.com', 'admin')"
