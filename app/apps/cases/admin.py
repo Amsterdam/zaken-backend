5a5d4dadb3f2f7bc49170dcd52fdf90058c64b5b
Peter Curet <petercuret@gmail.com>
2020-07-13 16:24:32 +0200

    Added models for cases

--- app/apps/cases/admin.py
+++ app/apps/cases/admin.py
@@ -1,3 +1,17 @@
+from apps.cases.models import Address, Case, Project
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(Case)
+class CaseAdmin(admin.ModelAdmin):
+    list_display = ("identification", "start_date", "end_date", "project", "address")
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ()
--- app/apps/cases/models.py
+++ app/apps/cases/models.py
@@ -1,3 +1,61 @@
 from django.db import models
 
-# Create your models here.
+
+class Address(models.Model):
+    bag_id = models.CharField(max_length=255, null=False, unique=True)
+    street_name = models.CharField(max_length=255, null=True, blank=True)
+    street_number = models.IntegerField(null=True, blank=True)
+    suffix_letter = models.CharField(max_length=1, null=True, blank=True)
+    suffix = models.CharField(max_length=4, null=True, blank=True)
+    postal_code = models.CharField(max_length=6, null=True, blank=True)
+    lat = models.FloatField(null=True, blank=True)
+    lng = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        if self.street_name:
+            return (
+                f"{self.street_name}"
+                f" {self.street_number}{self.suffix_letter}{self.suffix},"
+                f" {self.postal_code}"
+            )
+        return self.bag_id
+
+    def get(name):
+        return Address.objects.get_or_create(name=name)[0]
+
+    def save(self, *args, **kwargs):
+        # TODO: Do a BAG request and add the remaining data
+        return super().save(*args, **kwargs)
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False, unique=True)
+
+    def get(name):
+        return Project.objects.get_or_create(name=name)[0]
+
+    def __str__(self):
+        return self.name
+
+
+class Case(models.Model):
+    class Meta:
+        ordering = ["start_date"]
+
+    identification = models.CharField(max_length=255, null=True, blank=True)
+    start_date = models.DateField(auto_now=True)
+    end_date = models.DateField(null=True)
+    project = models.ForeignKey(
+        to=Project, null=False, on_delete=models.CASCADE, related_name="cases"
+    )
+    address = models.ForeignKey(
+        to=Address, null=False, on_delete=models.CASCADE, related_name="cases"
+    )
+
+    def get(identification):
+        return Case.objects.get_or_create(identification=identification)[0]
+
+    def __str__(self):
+        if self.identification:
+            return self.identification
+        return ""
