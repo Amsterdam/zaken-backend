97aeb9f6d47ddddbfd7c399bd691edc7118d0b8f
Peter Curet <petercuret@gmail.com>
2020-07-16 16:18:34 +0200

    Added better error handling to key auth permission

--- app/apps/users/auth_apps.py
+++ app/apps/users/auth_apps.py
@@ -6,14 +6,17 @@ class AppsKeyAuth(BasePermission):
     SECRET_KEY = None
 
     def has_permission(self, request, view):
-        assert self.SECRET_KEY, "SECRET_KEY is not set"
+        try:
+            assert self.SECRET_KEY, "SECRET_KEY is not set"
 
-        secret_key_request = request.META.get("HTTP_AUTHORIZATION", None)
-        assert secret_key_request, "Secret key cannot be retrieved from request"
+            secret_key_request = request.META.get("HTTP_AUTHORIZATION", None)
+            assert secret_key_request, "Secret key cannot be retrieved from request"
 
-        has_permission = secret_key_request == self.SECRET_KEY
+            has_permission = secret_key_request == self.SECRET_KEY
 
-        return has_permission
+            return has_permission
+        except AssertionError:
+            return False
 
 
 class TopKeyAuth(AppsKeyAuth):
