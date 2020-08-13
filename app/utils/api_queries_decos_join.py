6f3366cdfadb812d3d4681ada9f071ba6253247e
Peter Curet <petercuret@gmail.com>
2020-08-13 17:00:03 +0200

    Requests for nested items

--- app/utils/api_queries_decos_join.py
+++ app/utils/api_queries_decos_join.py
@@ -8,28 +8,36 @@ logger = logging.getLogger(__name__)
 
 
 @retry(stop=stop_after_attempt(3), after=after_log(logger, logging.ERROR))
-def get_decos_join_permit():
+def generic_decos_request(url):
     try:
-        print("Starting Decos Join Request")
-        url = (
-            "https://decosdvl.acc.amsterdam.nl:443/decosweb/aspx/api/v1/items/90642DCCC2DB46469657C3D0DF0B1ED7/COBJECTS?filter=TEXT8"
-            " eq 'Herengracht' and INITIALS eq '1'"
-        )
         headers = {"Accept": "application/itemdata"}
         username = settings.DECOS_JOIN_USERNAME
         password = settings.DECOS_JOIN_PASSWORD
-        print(url)
-        print(headers)
-        print(len(username))
-        print(len(password))
         response = requests.get(
-            url, headers=headers, timeout=5, auth=(username, password)
+            url, headers=headers, timeout=8, auth=(username, password)
         )
-        print("Response")
-        print(response)
-        print(response.json())
-        return response.json()
+        data = response.json()
+        return data
     except Exception as e:
         print(e)
-    print("Reached here")
+
     return {}
+
+
+def get_decos_join_permit():
+    print("Starting Decos Join Request")
+    url = (
+        "https://decosdvl.acc.amsterdam.nl:443/decosweb/aspx/api/v1/items/90642DCCC2DB46469657C3D0DF0B1ED7/COBJECTS?filter=TEXT8"
+        " eq 'Herengracht' and INITIALS eq '1'"
+    )
+    data = generic_decos_request(url)
+    print("First request")
+    print(data)
+    items = data.get("links", [])
+    items_urls = [item.get("href") for item in items]
+    print("Nested items urls")
+    print(items_urls)
+    data_items = [generic_decos_request(url) for url in items_urls]
+    print("All data items")
+    print(data_items)
+    return {"items": data_items}
