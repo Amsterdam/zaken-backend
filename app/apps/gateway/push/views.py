8688392b2be04d1b4304ed642211dc6f96c971fa
Peter Curet <petercuret@gmail.com>
2020-10-20 13:31:11 +0200

    Refactored to improve readability, added documentation to View

--- app/apps/gateway/push/views.py
+++ app/apps/gateway/push/views.py
@@ -27,66 +27,13 @@ from rest_framework.response import Response
 LOGGER = logging.getLogger(__name__)
 
 
-def create_case(identification, case_type, bag_id, start_date, end_date):
-    case, _ = Case.objects.get_or_create(identification=identification)
-    case_type = CaseType.get(case_type)
-    address = Address.get(bag_id)
-
-    case.start_date = start_date
-    case.end_date = end_date
-    case.case_type = case_type
-    case.address = address
-    case.save()
-
-    return case
-
-
-def create_legacy_states(states_data, case):
-    states = []
-    for state_data in states_data:
-        name = state_data.get("name")
-
-        # TODO: These should be renamed BWV instead of OpenZaak
-        state_type = OpenZaakStateType.get(name)
-        state, created = OpenZaakState.objects.get_or_create(
-            state_type=state_type,
-            case=case,
-            invoice_identification=state_data.get("invoice_identification"),
-        )
-
-        state.start_date = state_data.get("start_date")
-        state.end_date = state_data.get("end_date", None)
-        state.gauge_date = state_data.get("gauge_date", None)
-        state.save()
-
-        states.append(state)
-
-    return states
-
-
-def create_users(user_emails):
-    users = []
-
-    for user_email in user_emails:
-        user, _ = User.objects.get_or_create(email=user_email)
-        users.append(user)
-
-    return users
-
-
-def create_state(case, users):
-    case_state_type, _ = CaseStateType.objects.get_or_create(name=IN_PROGRESS)
-    case_state, _ = CaseState.objects.get_or_create(
-        case=case, status=case_state_type, state_date=datetime.now().date()
-    )
-
-    case_state.users.set(users)
-    case_state.save()
-
-    return case_state
-
-
 class PushViewSet(viewsets.ViewSet):
+    """
+    Populates data though a push from Top, after which an "In Progress" state is created.
+    A push from Top occurs when a case is added to an itinerary.
+    After migrating from BWV to Zaken we can remove most of this push, and Top can do a simpler (and more generic) CREATE request for adding a state.
+    """
+
     permission_classes = [IsAuthenticated | TopKeyAuth]
     serializer_class = PushSerializer
 
@@ -108,10 +55,12 @@ class PushViewSet(viewsets.ViewSet):
             users = data.get("users", [])
             states_data = data.get("states", [])
 
-            case = create_case(identification, case_type, bag_id, start_date, end_date)
-            legacy_states = create_legacy_states(states_data, case)
-            users = create_users(users)
-            state = create_state(case, users)
+            case = self.create_case(
+                identification, case_type, bag_id, start_date, end_date
+            )
+            legacy_states = self.create_legacy_states(states_data, case)
+            users = self.create_users(users)
+            state = self.create_state(case, users)
 
             return Response(
                 {
@@ -126,3 +75,58 @@ class PushViewSet(viewsets.ViewSet):
         except Exception as e:
             LOGGER.error(f"Could not process push data: {e}")
             raise APIException(f"Could not push data: {e}")
+
+    def create_case(self, identification, case_type, bag_id, start_date, end_date):
+        case, _ = Case.objects.get_or_create(identification=identification)
+        case_type = CaseType.get(case_type)
+        address = Address.get(bag_id)
+
+        case.start_date = start_date
+        case.end_date = end_date
+        case.case_type = case_type
+        case.address = address
+        case.save()
+
+        return case
+
+    def create_legacy_states(self, states_data, case):
+        states = []
+        for state_data in states_data:
+            name = state_data.get("name")
+
+            # TODO: These should be renamed BWV instead of OpenZaak
+            state_type = OpenZaakStateType.get(name)
+            state, created = OpenZaakState.objects.get_or_create(
+                state_type=state_type,
+                case=case,
+                invoice_identification=state_data.get("invoice_identification"),
+            )
+
+            state.start_date = state_data.get("start_date")
+            state.end_date = state_data.get("end_date", None)
+            state.gauge_date = state_data.get("gauge_date", None)
+            state.save()
+
+            states.append(state)
+
+        return states
+
+    def create_users(self, user_emails):
+        users = []
+
+        for user_email in user_emails:
+            user, _ = User.objects.get_or_create(email=user_email)
+            users.append(user)
+
+        return users
+
+    def create_state(self, case, users):
+        case_state_type, _ = CaseStateType.objects.get_or_create(name=IN_PROGRESS)
+        case_state, _ = CaseState.objects.get_or_create(
+            case=case, status=case_state_type, state_date=datetime.now().date()
+        )
+
+        case_state.users.set(users)
+        case_state.save()
+
+        return case_state
