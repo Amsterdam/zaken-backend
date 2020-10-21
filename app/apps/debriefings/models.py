8e0016d90e11c9041a543731ffb3d4948a99c30b
Peter Curet <petercuret@gmail.com>
2020-10-21 16:26:33 +0200

    Add timeline objects

--- app/apps/debriefings/models.py
+++ app/apps/debriefings/models.py
@@ -1,4 +1,4 @@
-from apps.cases.models import Case
+from apps.cases.models import Case, CaseTimelineSubject, CaseTimelineThread
 from apps.users.models import User
 from django.db import models
 
@@ -14,3 +14,19 @@ class Debriefing(models.Model):
     date_modified = models.DateTimeField(auto_now=True)
     hit = models.BooleanField(null=False)
     feedback = models.CharField(null=False, blank=False, max_length=255)
+
+    def save(self, *args, **kwargs):
+        # TODO: adding the timeline objects here is done for demo/prototyping purposes. Remove or improve later.
+
+        case_timeline_subject, _ = CaseTimelineSubject.objects.get_or_create(
+            case=self.case, subject="Debriefing"
+        )
+        case_timeline_thread, _ = CaseTimelineThread.objects.get_or_create(
+            subject=case_timeline_subject
+        )
+        # case_timeline_thread.authors = [self.author]
+        case_timeline_thread.parameters = {"Hit": "Ja" if self.hit else "Nee"}
+        case_timeline_thread.notes = self.feedback
+        case_timeline_thread.save()
+
+        super().save(*args, **kwargs)
