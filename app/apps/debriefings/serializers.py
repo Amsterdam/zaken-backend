ec8bcdf889874829dfa20f0822c60f4a345cd7da
Peter Curet <petercuret@gmail.com>
2020-10-22 10:36:16 +0200

    Just use the identification string here instead of the pk

--- app/apps/debriefings/serializers.py
+++ app/apps/debriefings/serializers.py
@@ -3,15 +3,12 @@ from rest_framework import serializers
 
 
 class DebriefingSerializer(serializers.ModelSerializer):
-    case_identification = serializers.CharField(
-        read_only=True, source="case.identification"
-    )
+    case = serializers.CharField(read_only=True, source="case.identification")
 
     class Meta:
         model = Debriefing
         fields = (
             "case",
-            "case_identification",
             "author",
             "date_added",
             "date_modified",
