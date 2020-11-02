b03b696631ba0efca383345926d3ce5ac7c59ea3
Peter Curet <petercuret@gmail.com>
2020-11-02 14:05:27 +0100

    Fix dependency

--- app/apps/addresses/migrations/0001_initial.py
+++ app/apps/addresses/migrations/0001_initial.py
@@ -8,9 +8,39 @@ class Migration(migrations.Migration):
     initial = True
 
     # TODO: Add dependencies to make sure the tests can be run
-    dependencies = []
+    dependencies = [
+        ("cases", "0020_auto_20201027_0846"),
+    ]
 
     operations = [
+        migrations.CreateModel(
+            name="Address",
+            fields=[
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("bag_id", models.CharField(max_length=255, unique=True)),
+                (
+                    "street_name",
+                    models.CharField(blank=True, max_length=255, null=True),
+                ),
+                ("number", models.IntegerField(blank=True, null=True)),
+                (
+                    "suffix_letter",
+                    models.CharField(blank=True, max_length=1, null=True),
+                ),
+                ("suffix", models.CharField(blank=True, max_length=4, null=True)),
+                ("postal_code", models.CharField(blank=True, max_length=7, null=True)),
+                ("lat", models.FloatField(blank=True, null=True)),
+                ("lng", models.FloatField(blank=True, null=True)),
+            ],
+        ),
         migrations.RunSQL(
             """
             INSERT INTO addresses_address (
@@ -62,33 +92,5 @@ class Migration(migrations.Migration):
             FROM
                 addresses_address;
         """,
-        ),
-        migrations.CreateModel(
-            name="Address",
-            fields=[
-                (
-                    "id",
-                    models.AutoField(
-                        auto_created=True,
-                        primary_key=True,
-                        serialize=False,
-                        verbose_name="ID",
-                    ),
-                ),
-                ("bag_id", models.CharField(max_length=255, unique=True)),
-                (
-                    "street_name",
-                    models.CharField(blank=True, max_length=255, null=True),
-                ),
-                ("number", models.IntegerField(blank=True, null=True)),
-                (
-                    "suffix_letter",
-                    models.CharField(blank=True, max_length=1, null=True),
-                ),
-                ("suffix", models.CharField(blank=True, max_length=4, null=True)),
-                ("postal_code", models.CharField(blank=True, max_length=7, null=True)),
-                ("lat", models.FloatField(blank=True, null=True)),
-                ("lng", models.FloatField(blank=True, null=True)),
-            ],
         ),
     ]
