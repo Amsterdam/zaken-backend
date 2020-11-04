52006ac7a43293c3156001af0e85403a1d0937dd
Xavier Bloemen <xavier.bloemen@teamrockstars.nl>
2020-11-04 11:13:07 +0100

    Update admin.py

--- app/apps/cases/admin.py
+++ app/apps/cases/admin.py
@@ -5,3 +5,7 @@ from django.contrib import admin
 @admin.register(Case)
 class CaseAdmin(admin.ModelAdmin):
     list_display = ("id", "identification", "start_date", "end_date", "address")
+
+
+admin.site.register(CaseState, admin.ModelAdmin)
+admin.site.register(CaseStateType, admin.ModelAdmin)
