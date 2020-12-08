947265a5bbc669cc54ec8573b024701abaf87816
Xavier Bloemen <xavier.bloemen@teamrockstars.nl>
2020-12-08 12:05:19 +0100

    Celery setup

---
+++ app/apps/health/tasks.py
@@ -0,0 +1,15 @@
+import time
+
+from apps.cases.models import Case
+from config.celery import app as celery_app
+
+
+@celery_app.task
+def do_some_queries():
+    time.sleep(10)
+    return Case.objects.count()
+
+
+@celery_app.task
+def query_every_five_mins():
+    pass
