705b083724c120f8322537d21fd8c25980dbe182
Peter Curet <petercuret@gmail.com>
2020-12-09 15:41:06 +0100

    Removes DebugRabbitMQHealthCheck

--- app/apps/health/apps.py
+++ app/apps/health/apps.py
@@ -6,7 +6,6 @@ class HealthConfig(AppConfig):
     name = "apps.health"
 
     def ready(self):
-        from .health_checks import BAGServiceCheck, DebugRabbitMQHealthCheck
+        from .health_checks import BAGServiceCheck
 
         plugin_dir.register(BAGServiceCheck)
-        plugin_dir.register(DebugRabbitMQHealthCheck)
