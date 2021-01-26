6e2d95e4d0583d6f5a12d98237330c80f4c61eb5
Peter Curet <petercuret@gmail.com>
2021-01-26 17:41:21 +0100

    Some simple code to create a case in OpenZaak

--- app/apps/cases/signals.py
+++ app/apps/cases/signals.py
@@ -1,5 +1,6 @@
 from apps.camunda.services import CamundaService
 from apps.cases.models import Case
+from apps.openzaak.helpers import create_open_zaak_case
 from django.db.models.signals import post_init, post_save
 from django.dispatch import receiver
 
@@ -15,5 +16,6 @@ def create_case_instance_in_camunda(sender, instance, created, **kwargs):
 @receiver(post_save, sender=Case)
 def create_case_instance_in_openzaak(sender, instance, created, **kwargs):
     if created:
-        # TODO: Add code when OpenZaak connection is ready
-        pass
+        create_open_zaak_case(
+            identification=instance.identification, description=instance.description
+        )
---
+++ app/apps/openzaak/helpers.py
@@ -0,0 +1,35 @@
+from datetime import date
+
+from django.conf import settings
+from zgw_consumers.api_models.base import factory
+from zgw_consumers.api_models.zaken import Zaak
+from zgw_consumers.constants import APITypes
+from zgw_consumers.models import Service
+from zgw_consumers.service import get_paginated_results
+
+
+def get_default_zaaktype():
+    ztc_client = Service.objects.filter(api_type=APITypes.ztc).get().build_client()
+    results = ztc_client.list("zaaktype", {"identificatie": settings.DEFAULT_TEAM})
+    default_zaaktype = results["results"][0]
+    return default_zaaktype
+
+
+def create_open_zaak_case(identification, description):
+    zaak_type = get_default_zaaktype()
+
+    today = date.today().strftime("%Y-%m-%d")
+    zaak_body = {
+        "identificatie": identification,
+        "omschrijving": description,
+        "zaaktype": zaak_type["url"],
+        "bronorganisatie": settings.DEFAULT_CATALOGUS_RSIN,
+        "verantwoordelijkeOrganisatie": settings.DEFAULT_CATALOGUS_RSIN,
+        "registratiedatum": today,
+        "startdatum": today,
+    }
+
+    ztc_client = Service.objects.filter(api_type=APITypes.zrc).get().build_client()
+    zaak = ztc_client.create("zaak", zaak_body)
+    result = factory(Zaak, zaak)
+    return result
