de980459e14c670bea6c1994c872114b0c4e5dc4
Xavier Bloemen <xavier.bloemen@teamrockstars.nl>
2021-01-27 23:25:02 +0100

    updated BPMN and fix signal debriefing

--- app/apps/debriefings/signals.py
+++ app/apps/debriefings/signals.py
@@ -11,4 +11,6 @@ def complete_camunda_task_create_debrief(sender, instance, created, **kwargs):
     task = CamundaService().get_task_by_task_name_id_and_camunda_id(
         "task_create_debrief", instance.case.camunda_id
     )
-    CamundaService().complete_task(task["id"], {"violation": {"value": "true"}})
+    CamundaService().complete_task(
+        task["id"], {"violation": {"value": instance.violation}}
+    )
