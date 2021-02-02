3e321d989581af5c07d64f370f21bead989e67f6
Peter Curet <petercuret@gmail.com>
2021-02-02 16:05:29 +0100

    Should fix current issues with mocking data

--- app/apps/cases/signals.py
+++ app/apps/cases/signals.py
@@ -1,3 +1,4 @@
+import logging
 import sys
 
 from apps.camunda.services import CamundaService
@@ -6,20 +7,23 @@ from apps.openzaak.helpers import create_open_zaak_case
 from django.db.models.signals import post_init, post_save
 from django.dispatch import receiver
 
+logger = logging.getLogger(__name__)
+
 
 @receiver(post_save, sender=Case, dispatch_uid="case_init_in_camunda")
 def create_case_instance_in_camunda(sender, instance, created, **kwargs):
-    if created:
-        if "test" not in sys.argv:
-            camunda_id = CamundaService().start_instance()
-            instance.camunda_id = camunda_id
-            instance.save()
+    if created and "test" not in sys.argv:
+        camunda_id = CamundaService().start_instance()
+        instance.camunda_id = camunda_id
+        instance.save()
 
 
 @receiver(post_save, sender=Case)
 def create_case_instance_in_openzaak(sender, instance, created, **kwargs):
-    if created:
-        if "test" not in sys.argv:
+    if created and "test" not in sys.argv:
+        try:
             create_open_zaak_case(
                 identification=instance.identification, description=instance.description
             )
+        except Exception as e:
+            logger.error(e)
