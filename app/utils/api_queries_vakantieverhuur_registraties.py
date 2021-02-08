d46b84fdd19e59ab59f6e753523c760767075b4d
Peter Curet <petercuret@gmail.com>
2021-02-08 17:21:35 -0400

    Added logging for debugging

--- app/utils/api_queries_vakantieverhuur_registraties.py
+++ app/utils/api_queries_vakantieverhuur_registraties.py
@@ -15,9 +15,13 @@ def get_vakantieverhuur_registration(registration_number):
     header = {"x-api-key": settings.VAKANTIEVERHUUR_REGISTRATIE_API_ACCESS_TOKEN}
     url = f"{settings.VAKANTIEVERHUUR_REGISTRATIE_API_URL}{registration_number}"
 
+    logger.info(header)
+    logger.info(url)
+
     response = requests.get(
         url=url,
         headers=header,
+        verify="/usr/local/share/ca-certificates/adp_rootca.crt",
     )
     response.raise_for_status()
 
@@ -35,6 +39,7 @@ def get_bsn_vakantieverhuur_registrations(bsn_number):
     response = requests.get(
         url=url,
         headers=header,
+        verify="/usr/local/share/ca-certificates/adp_rootca.crt",
     )
     response.raise_for_status()
 
@@ -52,6 +57,7 @@ def get_bag_vakantieverhuur_registrations(bag_id):
     response = requests.get(
         url=url,
         headers=header,
+        verify="/usr/local/share/ca-certificates/adp_rootca.crt",
     )
     response.raise_for_status()
 
