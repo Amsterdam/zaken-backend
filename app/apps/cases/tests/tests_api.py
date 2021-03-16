06dc9cfc07fd93c379eea3e903b1b2e79387928d
Peter Curet <petercuret@gmail.com>
2021-03-16 11:49:23 -0400

    Tests all filter criteria

--- app/apps/cases/tests/tests_api.py
+++ app/apps/cases/tests/tests_api.py
@@ -1,7 +1,7 @@
 import datetime
 
 from apps.addresses.models import Address
-from apps.cases.models import Case, CaseReason, CaseTeam
+from apps.cases.models import Case, CaseReason, CaseState, CaseStateType, CaseTeam
 from apps.summons.models import SummonType
 from django.core import management
 from django.urls import reverse
@@ -102,7 +102,157 @@ class CaseTeamReasonApiTest(APITestCase):
         self.assertEqual(len(data["results"]), 2)
 
 
-class CaseApiTest(APITestCase):
+class CaseListApiTest(APITestCase):
+    def setUp(self):
+        management.call_command("flush", verbosity=0, interactive=False)
+
+    def test_unauthenticated_get(self):
+        url = reverse("cases-list")
+        client = get_unauthenticated_client()
+        response = client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_authenticated_succeed(self):
+        url = reverse("cases-list")
+        client = get_authenticated_client()
+        response = client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_empty(self):
+        url = reverse("cases-list")
+        client = get_authenticated_client()
+        response = client.get(url)
+        results = response.data["results"]
+        self.assertEqual(results, [])
+
+    def test_get_results(self):
+        QUANTITY = 10
+        baker.make(Case, _quantity=QUANTITY)
+
+        url = reverse("cases-list")
+        client = get_authenticated_client()
+        response = client.get(url)
+        results = response.data["results"]
+        self.assertEqual(len(results), QUANTITY)
+
+    def test_filter_start_date(self):
+        DATE_A = datetime.datetime.now()
+        DATE_B = DATE_A - datetime.timedelta(days=2)
+        FILTER_PARAMETERS = {"startDate": DATE_A.strftime("%Y-%m-%d")}
+
+        baker.make(Case, start_date=DATE_A)
+        baker.make(Case, start_date=DATE_B)
+
+        url = reverse("cases-list")
+        client = get_authenticated_client()
+        response = client.get(url, FILTER_PARAMETERS)
+
+        results = response.data["results"]
+        self.assertEqual(len(results), 1)
+
+    def test_filter_open_cases(self):
+        FILTER_PARAMETERS = {"openCases": "true"}
+        CLOSED_CASES_QUANTITY = 10
+        OPEN_CASES_QUANTITY = 5
+
+        baker.make(
+            Case, end_date=datetime.datetime.now(), _quantity=CLOSED_CASES_QUANTITY
+        )
+        baker.make(Case, _quantity=OPEN_CASES_QUANTITY)
+
+        url = reverse("cases-list")
+        client = get_authenticated_client()
+        response = client.get(url, FILTER_PARAMETERS)
+
+        results = response.data["results"]
+        self.assertEqual(len(results), OPEN_CASES_QUANTITY)
+
+    def test_filter_closed_cases(self):
+        FILTER_PARAMETERS = {"openCases": "false"}
+        CLOSED_CASES_QUANTITY = 10
+        OPEN_CASES_QUANTITY = 5
+
+        baker.make(
+            Case, end_date=datetime.datetime.now(), _quantity=CLOSED_CASES_QUANTITY
+        )
+        baker.make(Case, _quantity=OPEN_CASES_QUANTITY)
+
+        url = reverse("cases-list")
+        client = get_authenticated_client()
+        response = client.get(url, FILTER_PARAMETERS)
+
+        results = response.data["results"]
+        self.assertEqual(len(results), CLOSED_CASES_QUANTITY)
+
+    def test_filter_team(self):
+        TEAM_A = "TEAM A"
+        TEAM_B = "TEAM B"
+
+        team_a = baker.make(CaseTeam, name=TEAM_A)
+        baker.make(CaseTeam, name=TEAM_B)
+
+        baker.make(Case, team=team_a)
+        url = reverse("cases-list")
+        client = get_authenticated_client()
+
+        FILTER_PARAMETERS = {"team": TEAM_A}
+        response = client.get(url, FILTER_PARAMETERS)
+
+        results = response.data["results"]
+        self.assertEqual(len(results), 1)
+
+    def test_filter_reason(self):
+        REASON_A = "Reason A"
+        REASON_B = "Reason B"
+
+        reason_a = baker.make(CaseReason, name=REASON_A)
+        baker.make(CaseReason, name=REASON_B)
+        baker.make(Case, reason=reason_a)
+
+        url = reverse("cases-list")
+        client = get_authenticated_client()
+
+        FILTER_PARAMETERS = {"reason": reason_a}
+        response = client.get(url, FILTER_PARAMETERS)
+
+        results = response.data["results"]
+        self.assertEqual(len(results), 1)
+
+    def test_filter_status_same_type(self):
+        """
+        Cases have same state type, should only return cases with open state
+        """
+        state_type = baker.make(CaseStateType)
+        baker.make(CaseState, status=state_type)
+        # Makes closed states
+        baker.make(CaseState, end_date=datetime.datetime.now(), status=state_type)
+        baker.make(CaseState, end_date=datetime.datetime.now(), status=state_type)
+
+        url = reverse("cases-list")
+        client = get_authenticated_client()
+
+        FILTER_PARAMETERS = {"openStatus": state_type.name}
+        response = client.get(url, FILTER_PARAMETERS)
+
+        results = response.data["results"]
+        self.assertEqual(len(results), 1)
+
+    def test_filter_status_different_states(self):
+        """ Each case has a different state type, should only return one case """
+        case_states = baker.make(CaseState, _quantity=10)
+
+        url = reverse("cases-list")
+        client = get_authenticated_client()
+
+        test_state = case_states[0]
+        FILTER_PARAMETERS = {"openStatus": test_state.status.name}
+        response = client.get(url, FILTER_PARAMETERS)
+
+        results = response.data["results"]
+        self.assertEqual(len(results), 1)
+
+
+class CaseCreatApiTest(APITestCase):
     def setUp(self):
         management.call_command("flush", verbosity=0, interactive=False)
 
