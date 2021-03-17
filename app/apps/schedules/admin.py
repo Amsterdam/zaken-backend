2f0df18388a9d1f0f0b6f22106f3835b71897896
Peter Curet <petercuret@gmail.com>
2021-03-17 15:45:46 -0400

    Added models to admin

--- app/apps/schedules/admin.py
+++ app/apps/schedules/admin.py
@@ -1,3 +1,14 @@
+from apps.schedules.models import (
+    DaySegment,
+    Priority,
+    Schedule,
+    ScheduleType,
+    WeekSegment,
+)
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(DaySegment, admin.ModelAdmin)
+admin.site.register(Priority, admin.ModelAdmin)
+admin.site.register(Schedule, admin.ModelAdmin)
+admin.site.register(ScheduleType, admin.ModelAdmin)
+admin.site.register(WeekSegment, admin.ModelAdmin)
