36c5304c1483f89699ed1758799b953bfab6a91e
Peter Curet <petercuret@gmail.com>
2021-03-17 15:32:35 -0400

    Added scheduling models and model unit tests

--- app/apps/schedules/models.py
+++ app/apps/schedules/models.py
@@ -1,3 +1,64 @@
+from apps.cases.models import Case, CaseTeam
 from django.db import models
 
-# Create your models here.
+
+class ScheduleType(models.Model):
+    name = models.CharField(max_length=255)
+    team = models.ForeignKey(to=CaseTeam, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+        unique_together = ["name", "team"]
+
+
+class WeekSegment(models.Model):
+    name = models.CharField(max_length=255)
+    team = models.ForeignKey(to=CaseTeam, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+        unique_together = ["name", "team"]
+
+
+class DaySegment(models.Model):
+    name = models.CharField(max_length=255)
+    team = models.ForeignKey(to=CaseTeam, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+        unique_together = ["name", "team"]
+
+
+class Priority(models.Model):
+    name = models.CharField(max_length=255)
+    team = models.ForeignKey(to=CaseTeam, on_delete=models.CASCADE)
+    weight = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+        unique_together = ["name", "team"]
+
+
+class Schedule(models.Model):
+    schedule_type = models.ForeignKey(to=ScheduleType, on_delete=models.CASCADE)
+    week_segment = models.ForeignKey(to=WeekSegment, on_delete=models.CASCADE)
+    day_segment = models.ForeignKey(to=DaySegment, on_delete=models.CASCADE)
+    priority = models.ForeignKey(to=Priority, on_delete=models.CASCADE)
+    case = models.ForeignKey(
+        to=Case, related_name="schedules", on_delete=models.CASCADE
+    )
+
+    class Meta:
+        unique_together = ["case", "schedule_type"]
---
+++ app/apps/schedules/tests/tests_models.py
@@ -0,0 +1,112 @@
+from apps.schedules.models import (
+    DaySegment,
+    Priority,
+    Schedule,
+    ScheduleType,
+    WeekSegment,
+)
+from django.core import management
+from django.test import TestCase
+from model_bakery import baker
+
+
+class ScheduleTypeModelTest(TestCase):
+    MODEL = ScheduleType
+
+    def setUp(self):
+        management.call_command("flush", verbosity=0, interactive=False)
+
+    def test_can_create_object(self):
+        self.assertEquals(self.MODEL.objects.count(), 0)
+
+        baker.make(self.MODEL)
+
+        self.assertEquals(self.MODEL.objects.count(), 1)
+
+    def test_assert_unique_together(self):
+        model_object = baker.make(self.MODEL)
+
+        with self.assertRaises(Exception):
+            baker.make(self.MODEL, name=model_object.name, team=model_object.team)
+
+
+class WeekSegmentModelTest(TestCase):
+    MODEL = WeekSegment
+
+    def setUp(self):
+        management.call_command("flush", verbosity=0, interactive=False)
+
+    def test_can_create_object(self):
+        self.assertEquals(self.MODEL.objects.count(), 0)
+
+        baker.make(self.MODEL)
+
+        self.assertEquals(self.MODEL.objects.count(), 1)
+
+    def test_assert_unique_together(self):
+        model_object = baker.make(self.MODEL)
+
+        with self.assertRaises(Exception):
+            baker.make(self.MODEL, name=model_object.name, team=model_object.team)
+
+
+class DaySegmentModelTest(TestCase):
+    MODEL = DaySegment
+
+    def setUp(self):
+        management.call_command("flush", verbosity=0, interactive=False)
+
+    def test_can_create_object(self):
+        self.assertEquals(self.MODEL.objects.count(), 0)
+
+        baker.make(self.MODEL)
+
+        self.assertEquals(self.MODEL.objects.count(), 1)
+
+    def test_assert_unique_together(self):
+        model_object = baker.make(self.MODEL)
+
+        with self.assertRaises(Exception):
+            baker.make(self.MODEL, name=model_object.name, team=model_object.team)
+
+
+class PriorityModelTest(TestCase):
+    MODEL = Priority
+
+    def setUp(self):
+        management.call_command("flush", verbosity=0, interactive=False)
+
+    def test_can_create_object(self):
+        self.assertEquals(self.MODEL.objects.count(), 0)
+
+        baker.make(self.MODEL)
+
+        self.assertEquals(self.MODEL.objects.count(), 1)
+
+    def test_assert_unique_together(self):
+        model_object = baker.make(self.MODEL)
+
+        with self.assertRaises(Exception):
+            baker.make(self.MODEL, name=model_object.name, team=model_object.team)
+
+
+class ScheduleModelTest(TestCase):
+    MODEL = Schedule
+
+    def setUp(self):
+        management.call_command("flush", verbosity=0, interactive=False)
+
+    def test_can_create_object(self):
+        self.assertEquals(self.MODEL.objects.count(), 0)
+
+        baker.make(self.MODEL)
+
+        self.assertEquals(self.MODEL.objects.count(), 1)
+
+    def test_assert_unique_together(self):
+        model_object = baker.make(self.MODEL)
+
+        with self.assertRaises(Exception):
+            baker.make(
+                self.MODEL, name=model_object.case, team=model_object.schedule_type
+            )
