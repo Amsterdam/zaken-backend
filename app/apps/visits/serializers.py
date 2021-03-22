f4025c681291d3bea6711e0b4a948d5014f208f4
Peter Curet <petercuret@gmail.com>
2021-03-22 15:49:47 -0400

    Endpoint that is compatible with creating a Visit from both Top and Zaken

--- app/apps/visits/serializers.py
+++ app/apps/visits/serializers.py
@@ -1,28 +1,50 @@
-from apps.users.models import User
 from apps.users.serializers import UserSerializer
 from apps.visits.models import Visit
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
+
+User = get_user_model()
 
 
 class VisitSerializer(serializers.ModelSerializer):
-    authors = UserSerializer(many=True, read_only=True)
+    authors = UserSerializer(many=True)
     author_ids = serializers.PrimaryKeyRelatedField(
-        queryset=User.objects.all(), write_only=True, source="authors", many=True
+        queryset=User.objects.all(),
+        write_only=True,
+        source="authors",
+        many=True,
+        required=False,
     )
+
+    def validate(self, data):
+        if data.get("author_ids", None) and data.get("authors", None):
+            raise serializers.ValidationError(
+                "Either author_ids or authors should be provided"
+            )
+        return data
+
+    def get_authors(self, validated_data):
+        try:
+            authors_data = validated_data.pop("authors")
+        except KeyError:
+            return []
+
+        author_emails = [author_data["email"] for author_data in authors_data]
+        authors = []
+
+        for author_email in author_emails:
+            author, _ = User.objects.get_or_create(email=author_email)
+            authors.append(author)
+
+        return authors
+
+    def create(self, validated_data):
+        authors = self.get_authors(validated_data)
+        visit = Visit.objects.create(**validated_data)
+        visit.authors.set(authors)
+
+        return visit
 
     class Meta:
         model = Visit
         fields = "__all__"
-
-
-class TopVisitSerializer(serializers.Serializer):
-    case_identification = serializers.CharField()
-    start_time = serializers.CharField()
-    observations = serializers.ListField(child=serializers.CharField(max_length=255))
-    situation = serializers.CharField()
-    authors = serializers.ListField(child=serializers.CharField(max_length=255))
-    can_next_visit_go_ahead = serializers.BooleanField(allow_null=True)
-    can_next_visit_go_ahead_description = serializers.CharField(allow_null=True)
-    suggest_next_visit = serializers.CharField(allow_null=True)
-    suggest_next_visit_description = serializers.CharField(allow_null=True)
-    notes = serializers.CharField(allow_null=True)
--- app/apps/visits/views.py
+++ app/apps/visits/views.py
@@ -9,50 +9,10 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Visit
-from .serializers import TopVisitSerializer, VisitSerializer
+from .serializers import VisitSerializer
 
 
 class VisitViewSet(ModelViewSet):
     permission_classes = [IsInAuthorizedRealm | TopKeyAuth]
     serializer_class = VisitSerializer
     queryset = Visit.objects.all()
-
-    @extend_schema(
-        request=TopVisitSerializer,
-        description="Add Visit from TOP",
-    )
-    @action(detail=False, methods=["post"])
-    def create_visit_from_top(self, request):
-        serializer = TopVisitSerializer(data=request.data)
-
-        if serializer.is_valid(raise_exception=True):
-            visit = Visit().create_from_top(serializer.data)
-            if visit:
-                visit.save()
-                response = Response(VisitSerializer(visit).data)
-                return response
-            raise ValidationError("Case does not exist")
-
-    @extend_schema(
-        request=TopVisitSerializer,
-        description="Update Visit from TOP",
-    )
-    @action(detail=False, methods=["post"])
-    def update_visit_from_top(self, request):
-        serializer = TopVisitSerializer(data=request.data)
-
-        if serializer.is_valid(raise_exception=True):
-            try:
-                visit = Visit.objects.get(
-                    case__identification=serializer.data["case_identification"],
-                    start_time=serializer.data["start_time"],
-                )
-                visit = visit.update_from_top(serializer.data)
-            except Visit.DoesNotExist:
-                visit = Visit().create_from_top(serializer.data)
-
-            if visit:
-                visit.save()
-                response = Response(VisitSerializer(visit).data)
-                return response
-            raise ValidationError("Case does not exist")
