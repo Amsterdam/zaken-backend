c41e34eb6e61367a8520219230e3260bf25efdec
Peter Curet <petercuret@gmail.com>
2021-03-31 13:16:33 -0400

    Tests for visits

--- app/apps/visits/serializers.py
+++ app/apps/visits/serializers.py
@@ -8,36 +8,19 @@ User = get_user_model()
 
 class VisitSerializer(serializers.ModelSerializer):
     authors = UserSerializer(many=True, required=False)
-    # author_ids = serializers.PrimaryKeyRelatedField(
-    #     queryset=User.objects.all(),
-    #     write_only=True,
-    #     source="authors",
-    #     many=True,
-    #     required=False,
-    # )
-
-    def validate(self, data):
-        print(data)
-        return data
 
     def get_authors(self, validated_data):
         authors_data = validated_data.pop("authors")
-        print("Getting authors")
-        print(authors_data)
         authors = []
 
         for author in authors_data:
             if isinstance(author, User):
-                print(f"Author exists {author}")
                 authors.append(author)
             else:
-                print(f"Author create {author}")
                 email = author.get("email")
                 author, _ = User.objects.get_or_create(email=email)
                 authors.append(author)
 
-        print("authors:")
-        print(authors)
         return authors
 
     def create(self, validated_data):
---
+++ app/apps/visits/tests/tests_api.py
@@ -0,0 +1,61 @@
+from apps.visits.models import Visit
+from django.core import management
+from django.urls import reverse
+from model_bakery import baker
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from app.utils.unittest_helpers import (
+    get_authenticated_client,
+    get_test_user,
+    get_unauthenticated_client,
+)
+
+
+class VisitApiTest(APITestCase):
+    def setUp(self):
+        management.call_command("flush", verbosity=0, interactive=False)
+
+    def test_unauthenticated_get(self):
+        url = reverse("visits-list")
+        client = get_unauthenticated_client()
+        response = client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_authenticated_get(self):
+        url = reverse("visits-list")
+        client = get_authenticated_client()
+        response = client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_authenticated_get_empty(self):
+        url = reverse("visits-list")
+        client = get_authenticated_client()
+
+        response = client.get(url)
+        data = response.json()
+
+        self.assertEquals(data["results"], [])
+
+    def test_authenticated_get_filled(self):
+        baker.make(Visit, _quantity=2)
+
+        url = reverse("teams-list")
+        client = get_authenticated_client()
+
+        response = client.get(url)
+        data = response.json()
+
+        self.assertEquals(len(data["results"]), 2)
+
+    def test_unauthenticated_post(self):
+        url = reverse("teams-list")
+        client = get_unauthenticated_client()
+        response = client.post(url, data={}, format="json")
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_authenticated_post_bad_request(self):
+        url = reverse("teams-list")
+        client = get_authenticated_client()
+        response = client.post(url, data={}, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
