a3252a81793f2b1475d2932d8febbcf130400555
Xavier Bloemen <xavier.bloemen@teamrockstars.nl>
2021-04-07 13:42:58 +0200

    Updated BPMN and fix list endpoint for debriefing and summon

--- app/apps/debriefings/views.py
+++ app/apps/debriefings/views.py
@@ -2,12 +2,12 @@ import logging
 
 from apps.debriefings.models import Debriefing
 from apps.debriefings.serializers import DebriefingCreateSerializer
-from rest_framework.mixins import CreateModelMixin
+from rest_framework.mixins import CreateModelMixin, ListModelMixin
 from rest_framework.viewsets import GenericViewSet
 
 logger = logging.getLogger(__name__)
 
 
-class DebriefingViewSet(GenericViewSet, CreateModelMixin):
+class DebriefingViewSet(GenericViewSet, CreateModelMixin, ListModelMixin):
     serializer_class = DebriefingCreateSerializer
     queryset = Debriefing.objects.all()
--- app/apps/summons/views.py
+++ app/apps/summons/views.py
@@ -3,13 +3,13 @@ import logging
 from apps.summons.models import Summon
 from apps.summons.serializers import SummonSerializer
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.mixins import CreateModelMixin
+from rest_framework.mixins import CreateModelMixin, ListModelMixin
 from rest_framework.viewsets import GenericViewSet
 
 logger = logging.getLogger(__name__)
 
 
-class SummonViewSet(GenericViewSet, CreateModelMixin):
+class SummonViewSet(GenericViewSet, CreateModelMixin, ListModelMixin):
     serializer_class = SummonSerializer
     queryset = Summon.objects.all()
     filter_backends = [DjangoFilterBackend]
