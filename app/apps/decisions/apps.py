4947808556c39df790209bf4adb7d3af60635fea
Xavier Bloemen <xavier.bloemen@teamrockstars.nl>
2021-04-07 17:00:35 +0200

    decision signal added and moved and renamed some BPMN's

--- app/apps/decisions/apps.py
+++ app/apps/decisions/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class DecisionsConfig(AppConfig):
     name = "apps.decisions"
+
+    def ready(self):
+        pass
--- app/apps/decisions/signals.py
+++ app/apps/decisions/signals.py
@@ -1,18 +1,26 @@
+from apps.camunda.services import CamundaService
 from apps.decisions.models import Decision
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
-@receiver(post_save, sender=Decision, dispatch_uid="decision")
+@receiver(post_save, sender=Decision, dispatch_uid="decision_complete_camunda_task")
 def update_decision_with_summon(sender, instance, created, **kwargs):
     """
     TODO: Temporary hook to update decision with a summon instance.
     This will be resolved when we support multiple summons.
-
-    TODO: This isn't called because this signals file isn't registered in the apps.py
     """
     if created:
         # TODO: create belastingdienst number
         if instance.case.summons.count() == 1:
             instance.summon = instance.case.summons.all()[0]
             instance.save()
+
+    task = CamundaService().get_task_by_task_name_id_and_camunda_id(
+        "task_create_decision", instance.case.camunda_id
+    )
+    if task:
+        CamundaService().complete_task(
+            task["id"],
+            {"type_besluit": {"value": instance.decision_type.camunda_option}},
+        )
