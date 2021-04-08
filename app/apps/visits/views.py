aeab41ba25685804092ac8d052f86a6bbc86f107
Maurice Guikema <mguikema@gmail.com>
2021-04-08 18:58:12 +0200

    fix: TOP visit added multiple times

--- app/apps/visits/views.py
+++ app/apps/visits/views.py
@@ -1,6 +1,9 @@
+from apps.cases.models import Case
 from apps.users.auth_apps import TopKeyAuth
 from keycloak_oidc.drf.permissions import IsInAuthorizedRealm
+from rest_framework import status
 from rest_framework.mixins import CreateModelMixin, ListModelMixin
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from .models import Visit
@@ -13,5 +16,11 @@ class VisitViewSet(GenericViewSet, CreateModelMixin, ListModelMixin):
     queryset = Visit.objects.all()
 
     def create(self, request):
-        print(request.data)
-        super().create(request)
+        try:
+            request.data.update(
+                {"case": Case.objects.get(identification=request.data["case"]).id}
+            )
+        except Exception:
+            # TODO Return valid response code if TOP task retry is fixed
+            return Response({"Case not found"}, status=status.HTTP_202_ACCEPTED)
+        return super().create(request)
