781af9bc202c0c0f690de974f18e4da6881cdaa4
Xavier Bloemen <xavier.bloemen@teamrockstars.nl>
2021-04-12 10:46:23 +0200

    revert PR

--- app/apps/visits/views.py
+++ app/apps/visits/views.py
@@ -1,9 +1,6 @@
-from apps.cases.models import Case
 from apps.users.auth_apps import TopKeyAuth
 from keycloak_oidc.drf.permissions import IsInAuthorizedRealm
-from rest_framework import status
 from rest_framework.mixins import CreateModelMixin, ListModelMixin
-from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from .models import Visit
@@ -16,11 +13,5 @@ class VisitViewSet(GenericViewSet, CreateModelMixin, ListModelMixin):
     queryset = Visit.objects.all()
 
     def create(self, request):
-        try:
-            request.data.update(
-                {"case": Case.objects.get(identification=request.data["case"]).id}
-            )
-        except Exception:
-            # TODO Return valid response code if TOP task retry is fixed
-            return Response({"Case not found"}, status=status.HTTP_202_ACCEPTED)
+        print(request.data)
         return super().create(request)
