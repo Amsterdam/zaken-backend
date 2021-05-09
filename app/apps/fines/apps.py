03d3a67cd54de4e248c32daab32845cbb6c8d49a
Xavier Bloemen <xavier.bloemen@teamrockstars.nl>
2021-05-10 01:03:44 +0200

    apps name config fix

--- app/apps/fines/apps.py
+++ app/apps/fines/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class FinesConfig(AppConfig):
-    name = "fines"
+    name = "apps.fines"
--- app/apps/openzaak/apps.py
+++ app/apps/openzaak/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class OpenzaakConfig(AppConfig):
-    name = "openzaak"
+    name = "apps.openzaak"
