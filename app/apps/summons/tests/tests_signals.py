6479dacc9ce6d20d2bb4f04a3ead3ada903a9632
Xavier Bloemen <xavier.bloemen@teamrockstars.nl>
2021-06-02 12:49:27 +0200

    Removed test that refrences removed code

--- app/apps/summons/tests/tests_signals.py
+++ app/apps/summons/tests/tests_signals.py
@@ -1,7 +1,4 @@
-from apps.summons.signals import (
-    create_summon_instance_in_camunda,
-    create_summon_instance_in_openzaak,
-)
+from apps.summons.signals import create_summon_instance_in_openzaak
 from django.core import management
 from django.db.models import signals
 from django.test import TestCase
@@ -14,11 +11,6 @@ class SummonsSignalsTest(TestCase):
     def __get_registered_functions__(self):
         """ Returns all receiver functions for post_save functions """
         return [receiver[1]() for receiver in signals.post_save.receivers]
-
-    def test_camunda_signal_connected(self):
-        """ Tests if the camunda signal is registered """
-        registered_functions = self.__get_registered_functions__()
-        self.assertIn(create_summon_instance_in_camunda, registered_functions)
 
     def test_open_zaak_signal_connected(self):
         """ Tests if the openzaak signal is registered """
