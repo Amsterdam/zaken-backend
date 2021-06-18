ba953009ad596e49e01eee0f34dcb807a20faf80
Maurice Guikema <mguikema@gmail.com>
2021-06-18 16:19:28 +0200

    fix: summon link to decision, bpmn fixes

--- app/apps/decisions/signals.py
+++ app/apps/decisions/signals.py
@@ -1,5 +1,6 @@
 from apps.camunda.services import CamundaService
 from apps.decisions.models import Decision
+from apps.summons.models import Summon
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -11,10 +12,13 @@ def update_decision_with_summon(sender, instance, created, **kwargs):
     This will be resolved when we support multiple summons.
     """
     if created:
-        # TODO: create belastingdienst number
-        if instance.case.summons.count() == 1:
-            instance.summon = instance.case.summons.all()[0]
-            instance.save()
+        task_variables = CamundaService().get_task_variables(instance.camunda_task_id)
+        if task_variables:
+            summon_id = task_variables.get("summon_id", {}).get("value", 0)
+            summon = Summon.objects.filter(id=summon_id).first()
+            if summon:
+                instance.summon = summon
+                instance.save()
 
         CamundaService().complete_task(
             instance.camunda_task_id,
