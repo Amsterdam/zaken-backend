bbd6dcc4259aff2609d949c06fe978bf55baa722
André Lambert <afj.lambert@gmail.com>
2021-07-12 11:22:15 +0200

    Improve admin views mainly for 'theme' relations

--- app/apps/cases/admin.py
+++ app/apps/cases/admin.py
@@ -47,11 +47,59 @@ class CitizenReportAdmin(admin.ModelAdmin):
     )
 
 
-admin.site.register(CaseProcessInstance, admin.ModelAdmin)
+@admin.register(CaseReason)
+class CaseReasonAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "theme",
+    )
+    list_filter = ("theme",)
+
+
+@admin.register(CaseProject)
+class CaseProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "theme",
+    )
+    list_filter = ("theme",)
+
+
+@admin.register(CaseCloseReason)
+class CaseCloseReasonAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "case_theme",
+        "result",
+    )
+    list_filter = (
+        "case_theme",
+        "result",
+    )
+
+
+@admin.register(CaseCloseResult)
+class CaseCloseResultAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "case_theme",
+    )
+    list_filter = ("case_theme",)
+
+
+@admin.register(CaseProcessInstance)
+class CaseProcessInstanceAdmin(admin.ModelAdmin):
+    list_display = (
+        "process_id",
+        "case",
+        "camunda_process_id",
+    )
+    search_fields = (
+        "process_id",
+        "camunda_process_id",
+    )
+
+
 admin.site.register(CaseStateType, admin.ModelAdmin)
 admin.site.register(CaseTheme, admin.ModelAdmin)
-admin.site.register(CaseProject, admin.ModelAdmin)
-admin.site.register(CaseReason, admin.ModelAdmin)
-admin.site.register(CaseCloseReason, admin.ModelAdmin)
 admin.site.register(CaseClose, admin.ModelAdmin)
-admin.site.register(CaseCloseResult, admin.ModelAdmin)
