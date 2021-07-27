3445b3699e7a507a32aec6140b200de1586d4ab1
Maurice Guikema <mguikema@gmail.com>
2021-07-27 17:32:21 +0200

    kc email to lowercase: tests

--- app/apps/users/auth.py
+++ app/apps/users/auth.py
@@ -1,10 +1,15 @@
+import logging
+
 from django.conf import settings
+from django.core.exceptions import SuspiciousOperation
 from drf_spectacular.contrib.rest_framework_simplejwt import SimpleJWTScheme
 from keycloak_oidc.auth import OIDCAuthenticationBackend
 from mozilla_django_oidc.contrib.drf import OIDCAuthentication
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .auth_dev import DevelopmentAuthenticationBackend
+
+LOGGER = logging.getLogger(__name__)
 
 
 class MyOIDCAuthenticationBackend(OIDCAuthenticationBackend):
@@ -13,15 +18,45 @@ class MyOIDCAuthenticationBackend(OIDCAuthenticationBackend):
         email = claims.get("email")
         if not email:
             return self.UserModel.objects.none()
-        return self.UserModel.objects.filter(email__iexact=email)
+        print("email")
+        print(email)
+        users = self.UserModel.objects.filter(email__iexact=email)
+        print(users)
+
+        return users
 
     def get_or_create_user(self, access_token, id_token, payload):
+        """Returns a User instance if 1 user is found. Creates a user if not found
+        and configured to do so. Returns nothing if multiple users are matched."""
         print("get_or_create_user")
+
         user_info = self.get_userinfo(access_token, id_token, payload)
         print(user_info)
 
-        user = super().get_or_create_user(access_token, id_token, payload)
-        return user
+        claims_verified = self.verify_claims(user_info)
+        if not claims_verified:
+            msg = "Claims verification failed"
+            raise SuspiciousOperation(msg)
+
+        # email based filtering
+        users = self.filter_users_by_claims(user_info)
+
+        if len(users) == 1:
+            return self.update_user(users[0], user_info)
+        elif len(users) > 1:
+            # In the rare case that two user accounts have the same email address,
+            # bail. Randomly selecting one seems really wrong.
+            msg = "Multiple users returned"
+            raise SuspiciousOperation(msg)
+        elif self.get_settings("OIDC_CREATE_USER", True):
+            user = self.create_user(user_info)
+            return user
+        else:
+            LOGGER.debug(
+                "Login failed: No user with %s found, and " "OIDC_CREATE_USER is False",
+                self.describe_user_by_claims(user_info),
+            )
+            return None
 
 
 if settings.LOCAL_DEVELOPMENT_AUTHENTICATION:
