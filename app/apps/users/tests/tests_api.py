d3d90ac40c34bdeb86ca0b2442758c34415287c0
Maurice Guikema <mguikema@gmail.com>
2021-07-27 18:17:01 +0200

    permissions: swagger tweaks

--- app/apps/users/tests/tests_api.py
+++ app/apps/users/tests/tests_api.py
@@ -17,13 +17,13 @@ class UserListViewTest(APITestCase):
     """
 
     def test_unauthenticated_get(self):
-        url = reverse("authors-list")
+        url = reverse("users-list")
         client = get_unauthenticated_client()
         response = client.get(url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_authenticated_get(self):
-        url = reverse("authors-list")
+        url = reverse("users-list")
         client = get_authenticated_client()
         response = client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
