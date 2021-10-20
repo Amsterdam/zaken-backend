a9ed5b3cd1adcd1dbab44ef00d252254ca7fb301
Maurice Guikema <mguikema@gmail.com>
2021-10-20 12:44:49 +0200

    camunda data migrations: case events emitter_type_id to worflow

--- app/deploy/docker-entrypoint.development.sh
+++ app/deploy/docker-entrypoint.development.sh
@@ -20,6 +20,9 @@ chmod -R 777 /static
 echo Apply migrations
 python manage.py migrate --noinput
 
+echo Change case events emitter_type_id to workflow
+python manage.py change_case_event_emitter_type_id_to_workflow
+
 echo Axes check
 python manage.py check
 
