39560d6f8d51e50fda836e7bf5894d53dd8b418d
Maurice Guikema <mguikema@gmail.com>
2021-11-03 16:01:26 +0100

    event admin helpers

--- app/apps/events/admin.py
+++ app/apps/events/admin.py
@@ -5,5 +5,13 @@ admin.site.register(
     CaseEvent,
     admin.ModelAdmin,
     readonly_fields=("date_created", "event_values"),
-    list_display=("id", "emitter"),
+    list_display=(
+        "id",
+        "emitter",
+        "emitter_id",
+        "emitter_type",
+        "type",
+        "date_created",
+    ),
+    list_filter=("date_created", "type", "emitter_id"),
 )
