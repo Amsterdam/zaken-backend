4fcb6044421477446b87208a9421bc68de485357
André Lambert <afj.lambert@gmail.com>
2021-12-01 18:10:17 +0100

    Util script added and better docs

--- e2e-tests/setup_or_reset_and_start.sh
+++ e2e-tests/setup_or_reset_and_start.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+docker-compose down
+
+docker volume rm $(docker volume ls -q)
+
+docker-compose -f ../docker-compose.test.yml build
+
+docker-compose run --rm zaak-gateway python manage.py migrate
+
+bash ../bin/setup_credentials.sh
+
 docker-compose run --rm zaak-gateway python manage.py shell -c "
 from django.contrib.auth import get_user_model
 get_user_model().objects.get_or_create(email='local.user@dev.com', first_name='local', last_name='user')"
@@ -12,3 +23,5 @@ group.permissions.add(Permission.objects.get(name='Create a new Case'))
 group.permissions.add(Permission.objects.get(name='Can perform a tasks'))
 user = User.objects.get(email='local.user@dev.com')
 user.groups.add(group)"
+
+docker-compose -f ../docker-compose.test.yml up
