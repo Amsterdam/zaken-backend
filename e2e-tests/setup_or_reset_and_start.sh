48b50faa0aab3aec3e6a100b328b3540f2ad3d6b
André Lambert <afj.lambert@gmail.com>
2021-12-02 13:35:56 +0100

    Fix ci/cd

--- e2e-tests/setup_or_reset_and_start.sh
+++ e2e-tests/setup_or_reset_and_start.sh
@@ -10,18 +10,6 @@ docker-compose run --rm zaak-gateway python manage.py migrate
 
 bash ../bin/setup_credentials.sh
 
-docker-compose run --rm zaak-gateway python manage.py shell -c "
-from django.contrib.auth import get_user_model
-get_user_model().objects.get_or_create(email='local.user@dev.com', first_name='local', last_name='user')"
-
-docker-compose run --rm zaak-gateway python manage.py shell -c "
-from apps.users.models import User, UserGroup
-from django.contrib.auth.models import Permission
-(group, _) = UserGroup.objects.get_or_create(name='PROJECTHANDHAVER', display_name='Projecthandhaver')
-group.permissions.add(Permission.objects.get(name='Close a Case (by performing the last task)'))
-group.permissions.add(Permission.objects.get(name='Create a new Case'))
-group.permissions.add(Permission.objects.get(name='Can perform a tasks'))
-user = User.objects.get(email='local.user@dev.com')
-user.groups.add(group)"
+./fix_models.sh
 
 docker-compose -f ../docker-compose.test.yml up
