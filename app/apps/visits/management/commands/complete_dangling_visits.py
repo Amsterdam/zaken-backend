04668e0bbfab6d6cc47c314732ca2a96e335c1c0
Maurice Guikema <mguikema@gmail.com>
2022-05-03 09:49:04 +0200

    WZ-1905: complete visits task added

--- app/apps/visits/management/commands/complete_dangling_visits.py
+++ app/apps/visits/management/commands/complete_dangling_visits.py
@@ -1,42 +1,28 @@
 import logging
 
 from apps.visits.models import Visit
-from apps.workflow.models import CaseUserTask
 from django.core.management.base import BaseCommand
-from django.db.models import OuterRef, Subquery
 
 logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-
-        last_tasks = CaseUserTask.objects.filter(
-            id=OuterRef("case_user_task_id"),
-            task_name="task_create_visit",
-            completed=False,
-        ).order_by("-created")
         uncompleted_visits = (
-            Visit.objects.exclude(case_user_task_id="-1")
-            .annotate(last_task=Subquery(last_tasks.values("task_name")))
-            .filter(last_task__isnull=False)
+            Visit.objects.filter(
+                case__tasks__task_name="task_create_visit",
+                case__tasks__completed=False,
+            )
+            .exclude(case_user_task_id="-1")
+            .distinct("case_user_task_id")
+            .order_by("case_user_task_id", "-start_time")
         )
-
-        # uncompleted_visits = (
-        #     Visit.objects.filter(
-        #         # completed=False,
-        #         case__tasks__task_name="task_create_visit",
-        #         case__tasks__completed=False,
-        #     )
-        #     .exclude(case_user_task_id="-1")
-        #     .distinct("case_user_task_id")
-        #     .order_by("case_user_task_id", "-start_time")
-        # )
-
-        print("command: unique_user_task_ids")
-        print("visits")
-        print(uncompleted_visits.count())
-        print(uncompleted_visits)
-        # for v in uncompleted_visits:
-        # v.completed = True
-        # v.save()
+        for v in uncompleted_visits:
+            task = v.case.tasks.filter(
+                task_name="task_create_visit",
+                completed=False,
+                id=int(v.case_user_task_id),
+            )
+            if task:
+                v.completed = True
+                v.save()
--- app/apps/visits/signals.py
+++ app/apps/visits/signals.py
@@ -3,25 +3,9 @@ from apps.workflow.models import CaseWorkflow
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-# @receiver(pre_save, sender=Visit, dispatch_uid="add_case_user_task_id_to_visit")
-# def add_case_user_task_id_to_visit(sender, instance, **kwargs):
-#     if kwargs.get("raw"):
-#         return
-#     # if instance.completed:
-#         # raise Exception("Visit already completed")
-#     task = instance.case.tasks.filter(
-#         task_name="task_create_visit",
-#         completed=False,
-#     ).first()
-#     if task:
-#         instance.case_user_task_id = str(task.id)
-#     else:
-#         raise Exception("No task found")
-
 
 @receiver(post_save, sender=Visit, dispatch_uid="complete_task_create_visit")
 def complete_task_create_visit(sender, instance, created, **kwargs):
-    print("complete_task_create_visit: completed")
     if kwargs.get("raw"):
         return
     if instance.completed:
@@ -31,9 +15,6 @@ def complete_task_create_visit(sender, instance, created, **kwargs):
             id=int(instance.case_user_task_id),
         ).first()
         if task:
-            print("HAS TASK")
-            print(instance)
-            print(task)
             CaseWorkflow.complete_user_task(
                 instance.case_user_task_id,
                 {
