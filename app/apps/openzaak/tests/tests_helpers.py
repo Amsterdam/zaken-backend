89ceea6b61c3333b9717e1e98a50ca7f0e884a68
Maurice Guikema <mguikema@gmail.com>
2022-10-17 09:04:32 +0200

    fix: open-zaak delete document, unittests fix

--- app/apps/openzaak/tests/tests_helpers.py
+++ app/apps/openzaak/tests/tests_helpers.py
@@ -160,8 +160,11 @@ class OpenZaakConnectionTests(OpenZaakBaseMixin, TestCase):
 
     @requests_mock.Mocker()
     def test_delete_document(self, m):
+        mock_service_oas_get(m, self.ZAAK_DOCUMENT_URL, "zrc")
+        mock_service_oas_get(m, f"{self.DOCUMENTEN_ROOT}/lock", "drc")
         mock_service_oas_get(m, self.DOCUMENTEN_ROOT, "drc")
         m.delete(self.ZAAK_DOCUMENT_URL, json=None, status_code=204)
+        m.post(f"{self.DOCUMENTEN_ROOT}/lock", json=None, status_code=200)
         m.delete(self.DOCUMENT_URL, json=None, status_code=204)
         SimpleUploadedFile("file.txt", b"file_content", content_type="text/plain")
         theme = baker.make(CaseTheme, name="mock_name")
