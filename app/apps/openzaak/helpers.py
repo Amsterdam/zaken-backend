ec3180b562c1f9e33cf270a3c6bc72a320824c8e
remyvdwereld <86827854+remyvdwereld@users.noreply.github.com>
2022-12-19 10:52:38 +0100

    No object dots in Python

--- app/apps/openzaak/helpers.py
+++ app/apps/openzaak/helpers.py
@@ -208,12 +208,12 @@ def create_document(instance, file, language="nld", informatieobjecttype=None):
     document_body = _build_document_body(file, language, informatieobjecttype)
     print("=> CREATE DOCUMENT => document_body BUILD")
     drc_client = Service.objects.filter(api_type=APITypes.drc).get().build_client()
-    print("=> DOCUMENT_BODY => identificatie =>", document_body.identificatie)
+    print("=> DOCUMENT_BODY => identificatie =>", document_body.get("identificatie"))
     print(
         "=> DOCUMENT_BODY => informatieobjecttype =>",
-        document_body.informatieobjecttype,
+        document_body.get("informatieobjecttype"),
     )
-    print("=> DOCUMENT_BODY => bestandsnaam =>", document_body.bestandsnaam)
+    print("=> DOCUMENT_BODY => bestandsnaam =>", document_body.get("bestandsnaam"))
     try:
         response = drc_client.create("enkelvoudiginformatieobject", document_body)
     except Exception as e:
