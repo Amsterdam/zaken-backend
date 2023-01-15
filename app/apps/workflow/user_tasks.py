8cdc47349fe6960d73aa161e3a716b4d36e5a845
remyvdwereld <86827854+remyvdwereld@users.noreply.github.com>
2023-01-15 23:23:43 +0100

    Test task

--- app/apps/workflow/user_tasks.py
+++ app/apps/workflow/user_tasks.py
@@ -241,14 +241,14 @@ class task_opstellen_concept_aanschrijving(user_task):
     print("=> TASK: task_opstellen_concept_aanschrijving e2e and workflow", user_task)
 
     def instance_created(self):
-        from apps.cases.models import CaseState
+        # from apps.cases.models import CaseState
 
         print("=> task_opstellen_concept_aanschrijving e2e and workflow 2.0", user_task)
 
-        CaseState.objects.get_or_create(
-            case=self.case_user_task.case,
-            status=CaseState.CaseStateChoice.HANDHAVING,
-        )
+        # CaseState.objects.get_or_create(
+        #     case=self.case_user_task.case,
+        #     status=CaseState.CaseStateChoice.HANDHAVING,
+        # )
         return
 
 
