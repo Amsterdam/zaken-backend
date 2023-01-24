66ae36409d32fabe1d65bef84bb1409ae204b5ca
remyvdwereld <86827854+remyvdwereld@users.noreply.github.com>
2023-01-24 21:53:20 +0100

    Test

--- app/apps/openzaak/signals.py
+++ app/apps/openzaak/signals.py
@@ -25,36 +25,37 @@ def create_case_instance_in_openzaak(sender, instance, created, **kwargs):
             logger.error(e)
         except Exception as e:
             logger.exception(e)
-    # else:
-    #     try:
-    #         update_open_zaak_case(instance)
-    #     except ClientError as e:
-    #         logger.error(e)
-    #     except Exception as e:
-    #         logger.exception(e)
 
 
 @receiver(
-    post_save, sender=CaseState, dispatch_uid="create_case_state_instance_in_openzaak"
+    post_save,
+    sender=CaseState,
+    dispatch_uid="set_result_and_create_case_instance_in_openzaak",
 )
-def create_case_state_instance_in_openzaak(sender, instance, created, **kwargs):
-    print("=> SIGNAL RECEIVED: CaseState", instance)
-    if not instance.case.case_url or instance.set_in_open_zaak or instance.system_build:
-        print("=> SIGNAL PASS")
-    elif instance.status == CaseState.CaseStateChoice.HANDHAVING:
-        print("=> SIGNAL HANDHAVING")
+def set_resultaat_and_create_case_instance_in_openzaak(
+    sender, instance, created, **kwargs
+):
+    # If the state of a case has changed from Toezicht to Handhaven,
+    # a new case instance must be created in open-zaak because of the limitation period.
+    # Toezicht has a limitation period of five years and Handhaven ten years.
+    if (
+        instance.case.case_url
+        and instance.status == CaseState.CaseStateChoice.HANDHAVING
+        and not instance.set_in_open_zaak
+        and not instance.system_build
+    ):
         try:
             # Set Resultaat "Toezicht uitgevoerd"
             create_open_zaak_case_resultaat(instance.case)
             # Set Status "Afsluiten" to close the case in open-zaak
             create_open_zaak_case_status(instance)
             # Get previous CaseState
-            previous_casestate = CaseState.objects.get(
+            previous_casestate_instance = CaseState.objects.get(
                 case=instance.case, status=CaseState.CaseStateChoice.TOEZICHT
             )
             # Update previous CaseState
-            previous_casestate.set_in_open_zaak = True
-            previous_casestate.save()
+            previous_casestate_instance.set_in_open_zaak = True
+            previous_casestate_instance.save()
             # Create new case in open-zaak with zaaktype HANDHAVING
             create_open_zaak_case(
                 instance.case,
@@ -64,48 +65,55 @@ def create_case_state_instance_in_openzaak(sender, instance, created, **kwargs):
             logger.error(e)
         except Exception as e:
             logger.exception(e)
-    elif instance.status == CaseState.CaseStateChoice.AFGESLOTEN:
-        print("=> SIGNAL AFGESLOTEN")
+
+
+@receiver(
+    post_save,
+    sender=CaseState,
+    dispatch_uid="set_resultaat_and_close_case_instance_in_openzaak",
+)
+def set_resultaat_and_close_case_instance_in_openzaak(
+    sender, instance, created, **kwargs
+):
+    # To close a case in open-zaak a Resultaat and Status must be created.
+    if (
+        instance.case.case_url
+        and instance.status == CaseState.CaseStateChoice.AFGESLOTEN
+        and not instance.set_in_open_zaak
+        and not instance.system_build
+    ):
+        previous_casestate_instance = CaseState.objects.filter(
+            case=instance.case, status=CaseState.CaseStateChoice.HANDHAVING
+        ).first()
+
         try:
-            previous_casestate = CaseState.objects.get(
-                case=instance.case, status=CaseState.CaseStateChoice.HANDHAVING
-            )
-            print("=> Afsluiten zaak in Handhaven")
-            # Case has status Handhaven so set Resultaat "Handhaven uitgevoerd"
-            create_open_zaak_case_resultaat(instance.case)
-            # Set Status "Afsluiten" to close the case in open-zaak
-            create_open_zaak_case_status(instance)
-            # Update previous CaseState
-            previous_casestate.set_in_open_zaak = True
-            previous_casestate.save()
-            # Update current instance of CaseState
-            instance.set_in_open_zaak = True
-            instance.save()
-        except CaseState.DoesNotExist:
-            print("=> Afsluiten zaak in Toezicht")
-            # Case has status Toezicht so set Resultaat "Toezicht afgebroken"
-            create_open_zaak_case_resultaat(
-                instance.case,
-                omschrijving_generiek=settings.OPENZAAK_RESULTAATTYPE_OMSCHRIJVING_GENERIEK_AFGEBROKEN,
-            )
-            # Set Status "Afsluiten" to close the case in open-zaak
-            create_open_zaak_case_status(instance)
-            # Get previous CaseState
-            previous_casestate = CaseState.objects.get(
-                case=instance.case, status=CaseState.CaseStateChoice.TOEZICHT
-            )
-            # Update previous CaseState
-            previous_casestate.set_in_open_zaak = True
-            previous_casestate.save()
-            # Update current instance of CaseState
-            instance.set_in_open_zaak = True
-            instance.save()
+            if previous_casestate_instance is None:
+                # Case has no CaseState Handhaving so set Resultaat "Toezicht afgebroken"
+                create_open_zaak_case_resultaat(
+                    instance.case,
+                    omschrijving_generiek=settings.OPENZAAK_RESULTAATTYPE_OMSCHRIJVING_GENERIEK_AFGEBROKEN,
+                )
+                # Get previous CaseState
+                previous_casestate_instance = CaseState.objects.get(
+                    case=instance.case, status=CaseState.CaseStateChoice.TOEZICHT
+                )
+            else:
+                # Case has a CaseState Handhaven so set Resultaat "Handhaven uitgevoerd"
+                create_open_zaak_case_resultaat(instance.case)
+
         except ClientError as e:
             logger.error(e)
         except Exception as e:
             logger.exception(e)
-    else:
-        print("=> SIGNAL ELSE")
+        finally:
+            # Set Status "Afsluiten" to close the case in open-zaak
+            create_open_zaak_case_status(instance)
+            # Update previous CaseState Toezicht or Handhaving
+            previous_casestate_instance.set_in_open_zaak = True
+            previous_casestate_instance.save()
+            # Update current instance of CaseState
+            instance.set_in_open_zaak = True
+            instance.save()
 
 
 @receiver(post_save, sender=CaseDocument)
