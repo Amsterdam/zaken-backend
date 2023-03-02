4a736ab12b43eb2b7efc573987d19cdea0a045dd
remyvdwereld <86827854+remyvdwereld@users.noreply.github.com>
2023-03-02 10:48:28 +0100

    62829

--- app/apps/workflow/views.py
+++ app/apps/workflow/views.py
@@ -73,7 +73,6 @@ class CaseUserTaskFilter(filters.FilterSet):
     role = filters.CharFilter(method="get_role")
     name = filters.ModelMultipleChoiceFilter(
         queryset=CaseUserTask.objects.filter(completed=False),
-        method="get_name",
         to_field_name="name",
     )
     theme = filters.ModelMultipleChoiceFilter(
@@ -116,13 +115,6 @@ class CaseUserTaskFilter(filters.FilterSet):
 
     def get_role(self, queryset, name, value):
         return queryset.filter(roles__contains=[value])
-
-    def get_name(self, queryset, name, value):
-        if value:
-            return queryset.filter(
-                workflow__tasks__in=value,
-            )
-        return queryset
 
     def get_ton_ids(self, queryset, name, value):
         return queryset.filter(case__ton_ids__contains=value)
