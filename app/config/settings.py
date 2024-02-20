4a8c6264593b730ff3395d9c3d0f36b6b4c35dd4
Nino van der Laan <n.van.der.laan@amsterdam.nl>
2024-02-20 13:39:43 +0100

    disable debug again

--- app/config/settings.py
+++ app/config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 ENVIRONMENT = os.getenv("ENVIRONMENT")
-DEBUG = True
+DEBUG = ENVIRONMENT == "development"
 
 # Settings to improve security
 is_secure_environment = not DEBUG
