21408b08440fd343a22de1550bf7b3290947f099
Nino van der Laan <n.van.der.laan@amsterdam.nl>
2024-02-20 10:00:25 +0100

    simplify for now

--- app/manage.py
+++ app/manage.py
@@ -2,9 +2,9 @@
 import os
 import sys
 from azure.monitor.opentelemetry import configure_azure_monitor
-from opentelemetry.instrumentation.django import DjangoInstrumentor
-from opentelemetry.instrumentation.logging import LoggingInstrumentor
-from opentelemetry.instrumentation.psycopg2 import Psycopg2Instrumentor
+# from opentelemetry.instrumentation.django import DjangoInstrumentor
+# from opentelemetry.instrumentation.logging import LoggingInstrumentor
+# from opentelemetry.instrumentation.psycopg2 import Psycopg2Instrumentor
 
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     configure_azure_monitor(
         connection_string=os.environ['APPLICATIONINSIGHTS_CONNECTION_STRING']
     )
-    LoggingInstrumentor().instrument(set_logging_format=True)
-    DjangoInstrumentor().instrument(is_sql_commentor_enabled=True)
-    Psycopg2Instrumentor().instrument()
+    # LoggingInstrumentor().instrument(set_logging_format=True)
+    # DjangoInstrumentor().instrument(is_sql_commentor_enabled=True)
+    # Psycopg2Instrumentor().instrument()
     execute_from_command_line(sys.argv)
