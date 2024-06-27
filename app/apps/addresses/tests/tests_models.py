b824da1dcc32eaf7b4a3e575caa9b08412ee7bf0
Remy van der Wereld <r.van.der.wereld@amsterdam.nl>
2024-06-27 10:46:44 +0200

    Updated tests

--- app/apps/addresses/tests/tests_models.py
+++ app/apps/addresses/tests/tests_models.py
@@ -3,8 +3,8 @@ from unittest.mock import patch
 from apps.addresses.mock import (
     mock_do_bag_search_id_result,
     mock_do_bag_search_id_result_without_links,
-    mock_get_bag_data_result,
-    mock_get_bag_data_result_without_stadsdeel,
+    mock_get_bag_identificatie_and_stadsdeel_result,
+    mock_get_bag_identificatie_and_stadsdeel_result_without_stadsdeel,
 )
 from apps.addresses.models import Address, District
 from django.core import management
@@ -22,21 +22,22 @@ class AddressModelTest(TestCase):
     def test_can_create_address(self):
         """Tests Address object creation without bag mocks"""
         self.assertEquals(Address.objects.count(), 0)
-
         baker.make(Address)
-
         self.assertEquals(Address.objects.count(), 1)
 
     @patch("apps.addresses.models.do_bag_search_by_bag_id")
+    @patch("apps.addresses.models.do_bag_search_benkagg_by_bag_id")
     def test_can_create_address_with_bag_result_without_verblijftobject_url(
-        self, mock_do_bag_search_id, mock_get_bag_data
+        self, mock_do_bag_search_id, mock_do_bag_search_benkagg_id
     ):
         """Tests Address object creation with bag data mocks without verblijftobject url"""
 
         mock_do_bag_search_id.return_value = (
             mock_do_bag_search_id_result_without_links()
         )
-        mock_get_bag_data.return_value = mock_get_bag_data_result()
+        mock_do_bag_search_benkagg_id.return_value = (
+            mock_get_bag_identificatie_and_stadsdeel_result()
+        )
 
         self.assertEquals(Address.objects.count(), 0)
         self.assertEquals(District.objects.count(), 0)
@@ -46,13 +47,16 @@ class AddressModelTest(TestCase):
         mock_do_bag_search_id.assert_called()
 
     @patch("apps.addresses.models.do_bag_search_by_bag_id")
+    @patch("apps.addresses.models.do_bag_search_benkagg_by_bag_id")
     def test_can_create_address_with_bag_result_without_verblijftobject_stadsdeel(
-        self, mock_do_bag_search_id, mock_get_bag_data
+        self, mock_do_bag_search_id, mock_do_bag_search_benkagg_id
     ):
-        """Tests Address object creation with bag data mocks without verblijftobject stadsdeel entry"""
+        """Tests Address object creation with bag data mocks without stadsdeel entry"""
 
         mock_do_bag_search_id.return_value = mock_do_bag_search_id_result()
-        mock_get_bag_data.return_value = mock_get_bag_data_result_without_stadsdeel()
+        mock_do_bag_search_benkagg_id.return_value = (
+            mock_get_bag_identificatie_and_stadsdeel_result_without_stadsdeel()
+        )
 
         self.assertEquals(Address.objects.count(), 0)
         self.assertEquals(District.objects.count(), 0)
@@ -62,13 +66,16 @@ class AddressModelTest(TestCase):
         mock_do_bag_search_id.assert_called()
 
     @patch("apps.addresses.models.do_bag_search_by_bag_id")
+    @patch("apps.addresses.models.do_bag_search_benkagg_by_bag_id")
     def test_can_create_address_with_bag_result(
-        self, mock_do_bag_search_id, mock_get_bag_data
+        self, mock_do_bag_search_id, mock_do_bag_search_benkagg_id
     ):
         """Tests Address object creation with bag data mocks"""
 
         mock_do_bag_search_id.return_value = mock_do_bag_search_id_result()
-        mock_get_bag_data.return_value = mock_get_bag_data_result()
+        mock_do_bag_search_benkagg_id.return_value = (
+            mock_get_bag_identificatie_and_stadsdeel_result()
+        )
 
         self.assertEquals(Address.objects.count(), 0)
         self.assertEquals(District.objects.count(), 0)
@@ -76,6 +83,7 @@ class AddressModelTest(TestCase):
         baker.make(Address)
 
         mock_do_bag_search_id.assert_called()
+        mock_do_bag_search_benkagg_id.assert_called()
 
         self.assertEquals(Address.objects.count(), 1)
         self.assertEquals(District.objects.count(), 1)
