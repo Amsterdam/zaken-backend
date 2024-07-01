b2e16fba345bdda1082f5f6df32572e074f3fbfa
Remy van der Wereld <r.van.der.wereld@amsterdam.nl>
2024-07-01 10:28:47 +0200

    Uncommented docker-entrypoint users_user

--- app/deploy/docker-entrypoint.development.sh
+++ app/deploy/docker-entrypoint.development.sh
@@ -15,7 +15,7 @@ python manage.py collectstatic --no-input
 chmod -R 777 /static
 
 # echo Clear tables
-# python manage.py shell -c "from django.db import connection; cursor = connection.cursor(); cursor.execute('drop table if exists "django_migrations" cascade; drop table if exists "django_content_type" cascade; drop table if exists "auth_permission" cascade; drop table if exists "auth_group" cascade; drop table if exists "auth_group_permissions" cascade; drop table if exists "users_user" cascade; drop table if exists "users_user_groups" cascade; drop table if exists "users_user_user_permissions" cascade; drop table if exists "django_admin_log" cascade; drop table if exists "django_session" cascade;'); cursor.close();"
+python manage.py shell -c "from django.db import connection; cursor = connection.cursor(); cursor.execute('drop table if exists "django_migrations" cascade; drop table if exists "django_content_type" cascade; drop table if exists "auth_permission" cascade; drop table if exists "auth_group" cascade; drop table if exists "auth_group_permissions" cascade; drop table if exists "users_user" cascade; drop table if exists "users_user_groups" cascade; drop table if exists "users_user_user_permissions" cascade; drop table if exists "django_admin_log" cascade; drop table if exists "django_session" cascade;'); cursor.close();"
 
 echo Apply migrations
 python manage.py migrate --noinput
