c8987834ce91d503d3992963b30e73c35e36d92d
Nino <n.van.der.laan@amsterdam.nl>
2024-08-05 15:53:16 +0200

    replace dc commands

--- e2e-tests/setup_or_reset_and_start.sh
+++ e2e-tests/setup_or_reset_and_start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-docker-compose down
+docker compose down
 
 docker volume rm $(docker volume ls -q)
 
-docker-compose -f ../docker-compose.local.yml build
+docker compose -f ../docker-compose.local.yml build
 
-docker-compose run --rm zaak-gateway python manage.py migrate
+docker compose run --rm zaak-gateway python manage.py migrate
 
 bash ../bin/setup_credentials.sh
 
 ./fix_models.sh
 
-docker-compose -f ../docker-compose.local.yml up
+docker compose -f ../docker-compose.local.yml up
