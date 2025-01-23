1c0120b66ba2ec4300bcb3f355f4a5d74717880c
Nino <n.van.der.laan@amsterdam.nl>
2025-01-23 14:13:52 +0100

    fix tests

--- app/utils/unittest_helpers.py
+++ app/utils/unittest_helpers.py
@@ -1,8 +1,22 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group, Permission
 from rest_framework.test import APIClient
 from rest_framework_simplejwt.tokens import RefreshToken
 
 AUTHENTICATED_CLIENT_EMAIL = "f.foo@foo.com"
+
+
+def add_user_to_authorized_groups(user):
+    """
+    Adds users to the authorized groups configured in the OIDC_AUTHORIZED_GROUPS
+    """
+    realm_access_groups = "all_permissions"
+    all_permissions = Permission.objects.all()
+    for realm_access_group in realm_access_groups:
+        group, _ = Group.objects.get_or_create(name=realm_access_group)
+        for permission in all_permissions:
+            group.permissions.add(permission)
+        group.user_set.add(user)
 
 
 def get_test_user():
@@ -17,6 +31,7 @@ def get_authenticated_client():
     Returns an authenticated APIClient, for unit testing API requests
     """
     user = get_test_user()
+    add_user_to_authorized_groups(user)
     access_token = RefreshToken.for_user(user).access_token
 
     client = APIClient()
