0a2ef95532fc5f025c4a4044bf88d4e95660f33d
Remy van der Wereld <r.van.der.wereld@amsterdam.nl>
2025-07-23 17:55:50 +0200

    Add new workflow option aanvullend huisbezoek

--- app/apps/visits/views.py
+++ app/apps/visits/views.py
@@ -1,5 +1,6 @@
 from apps.main.pagination import LimitedOffsetPaginator
 from apps.users.permissions import rest_permission_classes_for_top
+from apps.workflow.models import CaseWorkflow
 from rest_framework.mixins import CreateModelMixin, ListModelMixin
 from rest_framework.viewsets import GenericViewSet
 
@@ -12,3 +13,19 @@ class VisitViewSet(GenericViewSet, CreateModelMixin, ListModelMixin):
     serializer_class = VisitSerializer
     queryset = Visit.objects.all()
     pagination_class = LimitedOffsetPaginator
+
+    def perform_create(self, serializer):
+        case = serializer.validated_data.get("case")
+
+        # Check if this is an additional visit
+        is_additional = False
+        if case:
+            workflow = CaseWorkflow.objects.filter(case=case).first()
+            if (
+                workflow
+                and workflow.case_state_type
+                and "aanvullend" in workflow.case_state_type.name.lower()
+            ):
+                is_additional = True
+
+        serializer.save(is_additional=is_additional)
