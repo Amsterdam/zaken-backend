82128656af3109838868ceaf7ec3cb56ceda4479
Remy van der Wereld <r.van.der.wereld@amsterdam.nl>
2025-10-29 14:44:53 +0100

    Add last_login timestamp to user during save

--- app/apps/users/auth.py
+++ app/apps/users/auth.py
@@ -1,3 +1,4 @@
+import datetime
 import logging
 import time
 
@@ -16,6 +17,7 @@ class OIDCAuthenticationBackend(OIDCAuthenticationBackend):
     def save_user(self, user, claims):
         user.first_name = claims.get("given_name", "")
         user.last_name = claims.get("family_name", "")
+        user.last_login = datetime.datetime.now(datetime.timezone.utc)
         user.save()
         return user
 
