a3211130ca87a38001f365999c318d719642fd48
Koen Schaft <koen@schoenkaft.com>
2025-11-25 10:10:52 +0100

    Remove failing TestCongregateGateway test

--- e2e-tests/flows/test_congregate_gateway.py
+++ e2e-tests/flows/test_congregate_gateway.py
@@ -2,15 +2,12 @@ from api.config import Process, RenounceConceptSummon, SummonType, TypeConceptSu
 from api.tasks.debrief import test_opstellen_verkorte_rapportage_huisbezoek
 from api.tasks.summon import (
     test_afzien_concept_aanschrijving,
-    test_monitoren_binnenkomen_vergunningaanvraag,
-    test_monitoren_vergunningsprocedure,
     test_nakijken_aanschrijving,
     test_opstellen_concept_aanschrijving,
     test_verwerk_aanschrijving,
 )
 from api.tasks.visit import test_inplannen_status
 from api.test import DefaultAPITest
-from api.timers import WaitForTimer
 from api.validators import ValidateOpenTasks
 
 
@@ -55,34 +52,5 @@ class TestCongregateGateway(DefaultAPITest):
             test_verwerk_aanschrijving(
                 type=SummonType.Vakantieverhuur.WARNING_BB_LICENSE
             ),
-            ValidateOpenTasks(test_inplannen_status),
-        )
-
-    def test_two_summons_via_controleren_vergunningsprocedure(self):
-        """
-        TODO
-        """
-        # create case that will go to caseclose flow
-        case = self.get_case()
-        case.run_steps(
-            *test_monitoren_binnenkomen_vergunningaanvraag.get_steps(),
-        )
-        # add summon that will go to visit flow
-        case.add_process(Process.HolidayRental.ADD_SUMMON),
-
-        # finish first flow (to caseclose)
-        case.run_steps(
-            test_monitoren_vergunningsprocedure(),  # yes got permit
-        )
-
-        # finish second flow (to visit)
-        case.run_steps(
-            test_opstellen_concept_aanschrijving(
-                type_concept_summon=TypeConceptSummon.RENOUNCE_SUMMON
-            ),
-            test_afzien_concept_aanschrijving(
-                renounce_concept_summon=RenounceConceptSummon.NEW_VISIT_REQUIRED
-            ),
-            WaitForTimer(),
             ValidateOpenTasks(test_inplannen_status),
         )
