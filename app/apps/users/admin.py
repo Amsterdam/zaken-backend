bf8f808d5254643d07f0ffe6196e2b1105bd2b97
Nino <n.van.der.laan@amsterdam.nl>
2025-12-04 11:09:29 +0100

    Add export action users including groups

--- app/apps/users/admin.py
+++ app/apps/users/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import GroupAdmin, UserAdmin
 from django.contrib.auth.models import Group
+from django.http import HttpResponse
+from django.utils import timezone
+from openpyxl import Workbook
 
 from .models import ScopedViewToken, User, UserGroup
 
@@ -24,6 +27,53 @@ def assign_brp_gegevens_inzien(modeladmin, request, queryset):
         request,
         f"Assigned 'BRP_GEGEVENS_INZIEN' to {processed_users} selected user(s).",
     )
+
+
+@admin.action(description="Geselecteerde gebruikers exporteren inclusief groepen")
+def export_selected_users_to_excel(modeladmin, request, queryset):
+    wb = Workbook()
+    wb_sheet = wb.active
+    wb_sheet.title = "Gebruikers inclusief groepen"
+
+    headers = [
+        "Naam",
+        "E-mailadres",
+        "Groepen",
+        "Datum account aangemaakt",
+        "Laatste login",
+        "Actief",
+    ]
+    wb_sheet.append(headers)
+
+    for u in queryset.prefetch_related("groups"):
+        date_joined = (
+            timezone.localtime(u.date_joined).strftime("%d-%m-%Y %H:%M:%S")
+            if u.date_joined
+            else ""
+        )
+        last_login = (
+            timezone.localtime(u.last_login).strftime("%d-%m-%Y %H:%M:%S")
+            if u.last_login
+            else ""
+        )
+        full_name = f"{u.first_name} {u.last_name}".strip()
+        wb_sheet.append(
+            [
+                full_name,
+                u.email,
+                ", ".join(g.name for g in u.groups.all()),
+                date_joined,
+                last_login,
+                "Ja" if u.is_active else "Nee",
+            ]
+        )
+
+    response = HttpResponse(
+        content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
+    response["Content-Disposition"] = 'attachment; filename="gebruikers_groepen.xlsx"'
+    wb.save(response)
+    return response
 
 
 @admin.register(UserGroup)
