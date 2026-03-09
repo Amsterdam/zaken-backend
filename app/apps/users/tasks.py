973c8b925bb552575f6fcbea7bdb8cce3c28de30
Remy van der Wereld <r.van.der.wereld@amsterdam.nl>
2026-03-09 16:54:18 +0100

    Restrict inactive user deactivation to non-staff members

--- app/apps/users/tasks.py
+++ app/apps/users/tasks.py
@@ -11,7 +11,9 @@ Celery task to deactivate users who have not logged in for more than 6 months.
 def deactivate_inactive_users():
     User = get_user_model()
     six_months_ago = timezone.now() - timezone.timedelta(days=180)
-    inactive_users = User.objects.filter(is_active=True, last_login__lt=six_months_ago)
+    inactive_users = User.objects.filter(
+        is_active=True, last_login__lt=six_months_ago, is_staff=False
+    )
     for user in inactive_users:
         user.is_active = False
         user.save()
