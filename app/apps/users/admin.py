917310aae80baa42913bf74fedb6adf130aab48c
Remy van der Wereld <r.van.der.wereld@amsterdam.nl>
2026-03-09 10:53:35 +0100

    Remove 'BRP_GEGEVENS_INZIEN' role assignment action and update user list display; add task to deactivate inactive users

--- app/apps/users/admin.py
+++ app/apps/users/admin.py
@@ -10,25 +10,6 @@ from .models import ScopedViewToken, User, UserGroup
 admin.site.unregister(Group)
 
 
-@admin.action(description="Assign 'BRP_GEGEVENS_INZIEN' role")
-def assign_brp_gegevens_inzien(modeladmin, request, queryset):
-    """
-    Admin action to assign the 'BRP_GEGEVENS_INZIEN' role to selected users.
-    """
-    group, _ = UserGroup.objects.get_or_create(
-        name="BRP_GEGEVENS_INZIEN", defaults={"display_name": "BRP gegevens inzien"}
-    )
-    processed_users = 0
-    for user in queryset:
-        if not user.groups.filter(id=group.id).exists():
-            user.groups.add(group)
-        processed_users += 1
-    modeladmin.message_user(
-        request,
-        f"Assigned 'BRP_GEGEVENS_INZIEN' to {processed_users} selected user(s).",
-    )
-
-
 @admin.action(description="Geselecteerde gebruikers exporteren inclusief groepen")
 def export_selected_users_to_excel(modeladmin, request, queryset):
     wb = Workbook()
@@ -91,7 +72,7 @@ class UserGroupAdmin(GroupAdmin):
 
 @admin.register(User)
 class UserAdmin(UserAdmin):
-    actions = [assign_brp_gegevens_inzien, export_selected_users_to_excel]
+    actions = [export_selected_users_to_excel]
     fieldsets = (
         (
             "None",
@@ -113,7 +94,14 @@ class UserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {"classes": ("wide",), "fields": ("email", "password1", "password2")}),
     )
-    list_display = ("full_name", "email", "is_staff", "last_login", "date_joined")
+    list_display = (
+        "full_name",
+        "email",
+        "is_active",
+        "is_staff",
+        "last_login",
+        "date_joined",
+    )
     search_fields = ("email",)
     ordering = ("email",)
     readonly_fields = (
